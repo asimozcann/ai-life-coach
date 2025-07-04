74a5529b6b78872766b4152a00305689645c20e4
asimozcann <asm_ozcnn@outlook.com>
2025-07-04 18:43:49 +0300

    config dosyası düzenlendi

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: "esnext",
-    minify: "esbuild",
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (
-              id.includes("react") ||
-              id.includes("react-dom") ||
-              id.includes("react/jsx-runtime")
-            ) {
-              return "vendor-react";
-            }
-            if (id.includes("firebase")) return "vendor-firebase";
-            if (id.includes("axios")) return "vendor-axios";
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
+})
